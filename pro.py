f9ab3baea12299fa57e640270379c79650deb5d8
ll104567 <ll104567@163.com>
2019-02-02 10:42:30 +0800

    xx

--- pro.py
+++ pro.py
@@ -1,11 +1,22 @@
-class xx:
-	
-	def __init__(self,a):
-		self.a = a
-	
-	@property
-	def power(self):
-		return self.a ** 2
 
-a = xx(10)
-print(a.power)
+class Student(object):
+
+	_score = 0
+
+	@property
+	def score(self):
+
+		return self._score
+	
+	@score.setter
+	def score(self,value):
+		if not isinstance(value,int):
+			raise ValueError('Score must be int')
+		if value < 0 or value > 100:
+			raise ValueError('Score invaild')
+
+		self._score = value
+
+s = Student()
+s.score = 1002
+print(s.score)
